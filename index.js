bb977f70d824e40a6ee07b17dae8207211f53f89
„Eliška“ <eliska.foxx@gmail.com>
2023-12-10 15:58:19 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('funguju!');
+const budik = () => {
+    const alarm = document.querySelector(".alarm")
+    alarm.classList.add("alarm--ring")
+}
+let addTime = Number(prompt("Zadej počet vteřin:"))
+let addTimeMs = addTime * 1000
+const zazvon1 = setTimeout(budik, addTimeMs)
+
+let zvuk = document.querySelector(".zvuk")
+const play = () => {
+    zvuk.play()
+}
+const zazvon2 = setTimeout(play, addTimeMs)
+
+const zrusOdpocet1 = () => {
+    clearTimeout(zazvon1)
+}
+const tlacitko1 = document.querySelector(".btnElm1")
+tlacitko1.addEventListener("click", zrusOdpocet1)
+
+const zrusOdpocet2 = () => {
+    clearTimeout(zazvon2)
+}
+const tlacitko2 = document.querySelector(".btnElm2")
+tlacitko2.addEventListener("click", zrusOdpocet2)
